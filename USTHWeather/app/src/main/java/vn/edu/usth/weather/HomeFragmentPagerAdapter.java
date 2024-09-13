56bcdf1de9898167dc10214e60673784d7bc02e0
Duc Anh <anhbld.22bi13010@usth.edu.vn>
2024-09-13 13:49:15 +0700

    prac 5

---
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/HomeFragmentPagerAdapter.java
@@ -0,0 +1,37 @@
+package vn.edu.usth.weather;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class HomeFragmentPagerAdapter extends FragmentPagerAdapter {
+    private final int PAGE_COUNT = 3;
+    private String titles[] = new String[] { "Hanoi", "Paris", "Toulouse" };
+
+
+    public HomeFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public int getCount() {
+        return PAGE_COUNT;
+    }
+    @Override
+    public Fragment getItem(int page) {
+    // returns an instance of Fragment corresponding to the specified page
+        switch (page) {
+        case 0: return WeatherAndForecastFragment.newInstance("Paris", String.valueOf(R.drawable.weather_thunderstorm));
+        case 1: return WeatherAndForecastFragment.newInstance("Hanoi", String.valueOf(R.drawable.weather_rain));
+        case 2: return WeatherAndForecastFragment.newInstance("Toulouse", String.valueOf(R.drawable.weather_cloudy));
+    }
+    return new WeatherAndForecastFragment(); // failsafe
+    }
+
+    @Override
+    public CharSequence getPageTitle(int page) {
+    // returns a tab title corresponding to the specified page
+        return titles[page];
+    }
+
+    }
--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,57 +1,74 @@
 package vn.edu.usth.weather;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import android.util.Log;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
 
 public class WeatherActivity extends AppCompatActivity {
-    private static final String TAG = "WeatherActivity"; // Define TAG here
+
+    public static String TAG = "WeatherActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_weather);
+        setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        Log.i(TAG, "onCreate() called");
-        ForecastFragment forecastFragment = new ForecastFragment();
-        getSupportFragmentManager().beginTransaction().add(R.id.container, forecastFragment).commit();
-    }
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.i(TAG, "onStart() called");
+
+        PagerAdapter adapter = new HomeFragmentPagerAdapter( getSupportFragmentManager());
+        ViewPager pager = (ViewPager) findViewById(R.id.view_pager);
+        pager.setOffscreenPageLimit(3);
+        pager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(pager);
+
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.i(TAG, "onResume() called");
-    }
 
     @Override
-    protected void onPause() {
+    public void onPause()
+    {
         super.onPause();
-        Log.i(TAG, "onPause() called");
+        Log.i(TAG, "on pause");
     }
-
     @Override
-    protected void onStop() {
+    public void onStart()
+    {
+        super.onStart();
+        Log.i(TAG, "on start");
+    }
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        Log.i(TAG, "on resume");
+    }
+    @Override
+    public void onStop()
+    {
         super.onStop();
-        Log.i(TAG, "onStop() called");
+        Log.i(TAG, "on stop");
+    }
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+        Log.i(TAG, "on destroy");
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.i(TAG, "onDestroy() called");
-    }
 }
